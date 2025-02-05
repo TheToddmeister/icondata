49bacda3632cfb0e01d752ff82360f00366e6477
toddmeister <tormod_tho@hotmail.com>
2025-02-05 13:18:08 +0100

    .

---
+++ icondata_cf/src/lib.rs
@@ -0,0 +1,23 @@
+//! This crate provides a collection of icons in the form of SVG data
+//! from the [__Ant Design Icons__](https://github.com/ant-design/ant-design-icons) icon set.
+
+#[allow(non_upper_case_globals)]
+#[doc(hidden)]
+pub static CfNorway: &icondata_core::IconData = &icondata_core::IconData {
+    style: None,
+    x: None,
+    y: None,
+    width: None,
+    height: None,
+    view_box: Some("0 0 1024 1024"),
+    stroke_linecap: None,
+    stroke_linejoin: None,
+    stroke_width: None,
+    stroke: None,
+    fill: None,
+    data: r###"
+        <path fill="#ba0c2f" d="M0 0h22v16H0z"/>
+        <g fill="#fff"><path d="M6 0h4v16H6z"/><path d="M0 6h22v4H0z"/></g>
+        <g fill="#00205b"><path d="M7 0h2v16H7z"/><path d="M0 7h22v2H0z"/></g>
+     "###
+};
